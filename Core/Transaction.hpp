785a20d41d55535359e883a88552a9714ca596f1
ALEX <94417808+AlSch092@users.noreply.github.com>
2022-04-07 18:58:51 -0400

    Update Transaction.hpp

--- Core/Transaction.hpp
+++ Core/Transaction.hpp
@@ -4,13 +4,24 @@
 
 using namespace std;
 
+typedef unsigned char byte;
+
 class Transaction
 {
 public:
 
-	string GetData() { return this->sData; }
+	byte* GetData() { return this->bData; }
+
+	void Initialize(byte* bDataIn, uint32_t DataLen);
 
 private:
-	string sData;
 
+	byte* bData; //we should be using as little bytes as possible? stored as JSON text to represent each request?
+	uint32_t _nUsedBytes;
+
+
+	uint64_t _nPowderUsedByTransaction; //similar to 'gas' of ethereum
+	time_t _tTimestamp; //tx timestamp
+
+	uint32_t RequestedBy; //transaction author, similar to public key of msg.sender in eth
 };
