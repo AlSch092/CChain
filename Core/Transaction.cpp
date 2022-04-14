0d5b2c88481b97c319a7571394dc14210847047c
ALEX <94417808+AlSch092@users.noreply.github.com>
2022-04-13 22:45:21 -0400

    Update Transaction.cpp

--- Core/Transaction.cpp
+++ Core/Transaction.cpp
@@ -1,1 +1,12 @@
 #include "Transaction.hpp"
+
+void Transaction::Initialize(byte* bDataIn, uint32_t DataLen)
+{
+	this->bData = new byte[DataLen];
+
+}
+
+void Transaction::Destroy()
+{
+	delete[] this->bData;
+}
